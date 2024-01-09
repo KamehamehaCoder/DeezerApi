49127cc596b8245114a9816342a8a4e7e8cd2e8d
KamehamehaCoder <69098035+KamehamehaCoder@users.noreply.github.com>
2024-01-09 22:13:53 +0530

    Update ApiInterface.kt

--- app/src/main/java/com/namekianaffairs/deezerapi/ApiInterface.kt
+++ app/src/main/java/com/namekianaffairs/deezerapi/ApiInterface.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 
 interface ApiInterface {
 
-    @Headers("X-RapidAPI-Key: 8e33c8cb39msh014795be97f51d7p187ccbjsnf4702f240771",
+    @Headers("X-RapidAPI-Key: YOUR_API_HERE",
             "X-RapidAPI-Host: deezerdevs-deezer.p.rapidapi.com")
     @GET("search")
     fun getData(@Query("q")query: String) : Call<MyData>
